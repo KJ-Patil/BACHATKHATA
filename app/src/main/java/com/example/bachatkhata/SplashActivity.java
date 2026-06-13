04ffb0a71237be1b8b61f816a376e2e756b6596f
KJ-Patil <kiranjaware2017@gmail.com>
2026-06-13 14:15:46 +0530

    feat: add app icons, assets, and theme configurations for the Android project

---
+++ app/src/main/java/com/example/bachatkhata/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.bachatkhata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        // 4 seconds delay to transition to MainActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+    }
+}
